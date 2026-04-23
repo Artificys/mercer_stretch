573de442c5f3cd71a9cd5bb600dce0536df96a07
ZachNobles <zachnobles04@gmail.com>
2026-04-23 18:17:18 -0400

    switched tts engine to espeak

--- mercer_stretch/mercer_stretch/text_to_speech.py
+++ mercer_stretch/mercer_stretch/text_to_speech.py
@@ -2,7 +2,9 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
-import pyttsx3
+from voicebox.tts import ESpeakNG, ESpeakConfig
+from voicebox import SimpleVoicebox
+from voicebox.effects import Vocoder, Normalize
 from mercer_interfaces.srv import TextToSpeech
 
 class TextToSpeechNode(Node):
@@ -13,15 +15,15 @@ class TextToSpeechNode(Node):
 
         self.srv = self.create_service(TextToSpeech, "text_to_speech", self.text_to_speech_callback)
 
-        self.engine = pyttsx3.init()
-        self.engine.say("text to speech node is online")
-        self.engine.runAndWait()
+        engine_config = ESpeakConfig(speed = 120, pitch = 30, voice = "en-us")
+        engine = ESpeakNG(config=engine_config)
+        self.voicebox = SimpleVoicebox(engine, effects=[Vocoder.build(), Normalize()])
+        self.voicebox.say("text to speech node is online")
 
     
     def text_to_speech_callback(self, request, response):
         self.get_logger().info(f"text to speech service got request: {request.message}")
-        self.engine.say(request.message)
-        self.engine.runAndWait()
+        self.voicebox.say(request.message + " ")
         response.result = 0
         return response
 
