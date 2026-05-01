3ee4217c8e1fac24229d3c7707fa3277862f2345
ZachNobles <zachnobles04@gmail.com>
2026-05-01 16:21:54 -0400

    switched to faster AI model

--- mercer_stretch/mercer_stretch/speech_recognition.py
+++ mercer_stretch/mercer_stretch/speech_recognition.py
@@ -32,21 +32,24 @@ class SpeechRecognitionNode(Node):
         self.on_tour = False
         self.loading = False
         self.responding = False
-        
-        self.client = genai.Client(api_key = key)
-        self.chat = self.client.chats.create(model="gemma-4-31b-it")
-        test_response = self.chat.send_message("Testing. Please respond with \"Online\"")
-        self.get_logger().info(f"Received response to test prompt: {test_response.text}")
-        if len(test_response.text) > 0:
-            response = self.chat.send_message("""You are a Stretch RE1 Robot located in the Mercer Lab, a lab for the Department of Electrical, Computer, and Systems Engineering at Rensselaer Polytechnic Institute.
+
+        system_instructions = """You are a Stretch RE1 Robot located in the Mercer Lab, a lab for the Department of Electrical, Computer, and Systems Engineering at Rensselaer Polytechnic Institute.
             Soldering kits are available if you ask the storeroom worker. The storeroom is located at the entrance. Benchtop equipment including oscilloscopes, power supplies, and function generators are available at the worktables in the back. There are PCB printers on the right side. Resistors and some 74 series chips are available on the table by the PCB printers.
             Spools of wire and jumper cables are available at the back of the lab by the patent wall.
             Your responses will be put through a text to speech engine, so respond concisely when possible and avoid formatting such as bold that will be read as asterisks.
             If a user asks for a tour of the lab, respond with $CMD_TOUR. Do not start responses with $CMD unless a specified command is prompted.
             If a user tells you to home the robot and includes the word execute in their prompt, respond with $CMD_HOME.
             If a user tells you to stow the robot and includes the word execute in their prompt, respond with $CMD_STOW.
-            Respond with \"understood\"
-            """)
+            Respond with understood.
+            """
+        
+        self.client = genai.Client(api_key = key)
+        self.chat = self.client.chats.create(model="gemma-3n-e4b-it")
+        test_response = self.chat.send_message("Testing. Please respond with \"Online\"")
+        self.get_logger().info(f"Received response to test prompt: {test_response.text}")
+
+        if len(test_response.text) > 0:
+            response = self.chat.send_message(system_instructions)
             self.get_logger().info(f"Received response to information prompt: {response.text}")
         
         # Adjust for ambient noise
@@ -79,7 +82,7 @@ class SpeechRecognitionNode(Node):
             self.get_logger().info("Listening for speech...")
             while rclpy.ok():
                 if self.on_tour or self.loading: return
-                
+
                 try:
                     audio = self.recognizer.listen(source, timeout=2, phrase_time_limit=10)
                     self.get_logger().info("Processing audio...")
